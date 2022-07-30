ec5491b7a2854914cde7893b8f49b428dbc5091f
Julio Fuentes <juliodddddd1234@gmail.com>
2022-07-30 14:34:18 -0600

    A test for the logo was added

--- src/components/header/iconHeader.jsx
+++ src/components/header/iconHeader.jsx
@@ -10,7 +10,7 @@ const IconHeader = () => {
     return (
         <header className="text-[2rem] text-primary w-full overflow-x-hidden">
             <div className="absolute z-10 flex justify-between items-center w-full">
-                <span className="m-[1rem]">Icon</span>
+                <div className="flex justify-center items-center border-r-2 border-b-2 text-primary m-[1rem] w-20 h-20 logo"><span>F</span></div>
                 {!showMenu ? <MenuOpenIcon onClick={() => setShowMenu(!showMenu)} style={{ 'fontSize': '3rem', 'margin': '1rem' }} /> : <CloseIcon onClick={() => setShowMenu(!showMenu)} style={{ 'fontSize': '3rem', 'margin': '1rem' }} />}
             </div>
             <div className={`flex justify-end w-[100%] transition-{translate} ease duration-1000 ${showMenu ? 'translate-x-[0%]' : 'translate-x-[100%]'}`}>
